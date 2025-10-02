88992e17f4d950e6a1da01eb468a5fad097175d6
Mostafa_Akep <bebo86384@gmail.com>
2025-10-02 21:55:55 +0300

    commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,4 @@
-import 'package:avanced_animation/dismissible_with_drag_and_drop.dart';
+import 'package:avanced_animation/dismissible_widget.dart';
 import 'package:avanced_animation/dots_loading_animation.dart';
 import 'package:avanced_animation/interactive_widget.dart';
 import 'package:flutter/material.dart';
